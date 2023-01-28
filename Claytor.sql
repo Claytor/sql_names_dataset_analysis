644d76374418dff34895f0abcc55c866fda99dc2
Claytor <tim@claytor.me>
2023-01-28 14:08:35 -0600

    Got up to 16

--- Claytor.sql
+++ Claytor.sql
@@ -39,10 +39,10 @@ GROUP BY gender;
 -- There are more female registrations
 
 --8  What are the most popular male and female names overall (i.e., the most total registrations)?
-SELECT name, gender, SUM(num_registered) AS gender_reveal
+SELECT name, gender, SUM(num_registered) AS name_affirming
 FROM names
 GROUP BY name, gender
-ORDER BY gender_reveal DESC;
+ORDER BY name_affirming DESC;
 -- James and Mary
 
 --9 What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)?
@@ -62,12 +62,67 @@ ORDER BY spice_of_life DESC
 LIMIT 5;
 -- 2008 had the most variety
 
---------------------- He's dead, Jim! _____________________________
 --11  What is the most popular name for a girl that starts with the letter X?
 SELECT name, SUM(num_registered) AS x_women
 FROM names
 WHERE gender = 'F'
-	AND name LIKE 'x%'
+	AND name LIKE 'X%'
 GROUP BY name
 ORDER BY x_women DESC
 LIMIT 5;
+-- Ximena is the most popular
+
+--12. How many distinct names appear that start with a 'Q', but whose second letter is not 'u'?
+SELECT COUNT(DISTINCT name) as byebye_quincy
+FROM names 
+WHERE name LIKE 'Q%' 
+	AND name NOT LIKE '_u%';  
+--
+
+--13. Which is the more popular spelling between "Stephen" and "Steven"? Use a single query to answer this question.
+SELECT name, SUM(num_registered) AS i_prefer_stehphan
+FROM names 
+WHERE name = 'Stephen' OR name = 'Steven'
+GROUP BY name
+ORDER BY i_prefer_stehphan DESC;
+-- Steven is the the most popular spelling
+
+--14. What percentage of names are "unisex" - that is what percentage of names have been used both for boys and for girls?
+/*SELECT
+ (SELECT COUNT(*)
+   FROM (SELECT name
+   FROM names
+   GROUP BY name
+   HAVING COUNT(DISTINCT gender ) = 2) AS sub) /
+   CAST( COUNT(DISTINCT name) AS DOUBLE PRECISION)AS perc_unisex
+FROM names */
+--
+
+--15. How many names have made an appearance in every single year since 1880?
+SELECT name, COUNT(year) AS ancient_ones
+FROM names 
+GROUP BY name
+ORDER BY ancient_ones DESC
+LIMIT 15;
+-- 15 names appeared in all years
+
+--16. How many names have only appeared in one year?
+SELECT name, COUNT(year) AS rare_ones
+FROM names 
+GROUP BY name
+ORDER BY rare_ones
+--LIMIT 15;
+--
+
+--17. How many names only appeared in the 1950s?
+--
+
+--18. How many names made their first appearance in the 2010s?
+--
+
+--19. Find the names that have not be used in the longest.
+--
+
+--20. Come up with a question that you would like to answer using this dataset. Then write a query to answer this question.
+--
+
